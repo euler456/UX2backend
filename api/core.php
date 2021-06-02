d5fc90ba0516b21deb33d9053ff81e3347231886
euler5926 <euler5926@gmail.com>
2021-06-02 15:41:43 +1000

    ddd

--- api/core.php
+++ api/core.php
@@ -1,5 +1,5 @@
 <?php
-if (isset($_SERVER['HTTP_ORIGIN'])) { 
+/*if (isset($_SERVER['HTTP_ORIGIN'])) { 
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');    // cache for 1 day
 }
@@ -8,5 +8,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); 
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-}
+}*/
 ?>
