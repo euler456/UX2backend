b423c568d2bbc8d59d84e32fab65d2cb07c124fa
euler5926 <euler5926@gmail.com>
2021-06-03 11:13:34 +1000

    dfg

--- api/api.php
+++ api/api.php
@@ -84,8 +84,7 @@ elseif ($request->cookies->has('PHPSESSID')) {
                 $response->setStatusCode(400);
             }
         } elseif ($request->query->getAlpha('action') == 'login') {
-          
-            $res -> $session->logEvent($request->getClientIp(),'login',$request->cookies->get('PHPSESSID'));
+            $res =$session->get('sessionObj')->logEvent($request->getClientIp(),'login',$request->cookies->get('PHPSESSID'));
             if ($request->request->has('username') and $request->request->has('password')) {
                 $res = $session->get('sessionObj')->login(
                     $request->request->get('username'),
